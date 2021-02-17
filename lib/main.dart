edaa5a935babbd4d0b7c9d48c45d354cad37f81d
agrwl-prankit <agrwnlprankit77@gmail.com>
2021-02-17 20:32:52 +0530

    add custom list view

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:expense_planner/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main() => runApp(MyApp());
 
@@ -43,22 +44,27 @@ class MyHomePage extends StatelessWidget {
                   child: Row(
                     children: <Widget>[
                       Container(
-                        margin: EdgeInsets.symmetric(
-                            vertical: 10,
-                            horizontal: 15
-                        ),
+                        margin:
+                            EdgeInsets.symmetric(vertical: 10, horizontal: 15),
                         decoration: BoxDecoration(
-                            border: Border.all(
-                                color: Colors.black,
-                                width: 2
-                            )
-                        ),
-                        child: Text(tx.amount.toString()),
+                            border: Border.all(color: Colors.purple, width: 2)),
+                        padding: EdgeInsets.all(10),
+                        child: Text('\$ ${tx.amount.toString()}',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 15,
+                                color: Colors.purple)),
                       ),
                       Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: <Widget>[
-                          Text(tx.title),
-                          Text(tx.date.toString())
+                          Text(tx.title,
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.bold)),
+                          Text(
+                            DateFormat.yMMMd().format(tx.date),
+                            style: TextStyle(color: Colors.grey),
+                          )
                         ],
                       )
                     ],
@@ -70,3 +76,12 @@ class MyHomePage extends StatelessWidget {
         ));
   }
 }
+
+/*
+Container: only 1 child widget, rich alignment and styling options,
+flexible width (parent and child), perfect for custom styling and
+alignment
+Column/Row: multiple child widget, alignment but no styling, always
+takes full available height(column)/ width(row), must use if widget
+sits next to/ above to each other
+ */
